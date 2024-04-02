c0993b38ff9a6933ef1682eaea82a4b881922880
Tien <tieeeeen1994@gmail.com>
2024-04-02 08:56:24 +0800

    initial commit

---
+++ lib/breadcrumb_helper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'breadcrumb_helper/version'
+
+module BreadcrumbHelper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/breadcrumb_helper_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe BreadcrumbHelper do
+  it 'has a version number' do
+    expect(BreadcrumbHelper::VERSION).not_to be_nil
+  end
+end
